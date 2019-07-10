d0fc12bf81b8ab2da374dc052b88fd58da0dfc7d
young31 <migael38317@gmail.com>
2019-07-10 17:08:08 +0900

    Learn jinja2

--- startCamp/03_day/flask/intro/app.py
+++ startCamp/03_day/flask/intro/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import datetime
 
 app = Flask(__name__)
 
+### templates // app
+
+
 @app.route("/") # endpoint(데코레이터)
 def hello():
-    return "Hello Jason!"
+    # return "Hello Jason!"
+    return render_template('index.html') # render_teplate를 실행시켜서 보여줄 html
 
 @app.route('/ssafy')
 def ssafy():
@@ -34,11 +38,12 @@ def html_2():
 # variable routing
 @app.route('/greeting/<name>') # endpoint를 변수로 받기 >> default == str.type
 def greeting(name):
-    return f'hi {name} !'
+    return render_template('greeting.html', html_name=name)
 
 @app.route('/cube/<int:num>') 
 def cube(num):
-    return f'{num}의 3제곱은 {num ** 3}'
+    # return f'{num}의 3제곱은 {num ** 3}'
+    return render_template('cube.html', num_h=num, num_h3=num**3)
 
 # 실습
 @app.route('/lunch/<int:people>')
@@ -49,6 +54,13 @@ def lunch(people):
         return 'exceed no.'
     else:
         return f'{random.sample(menu,people)}'
+
+
+@app.route('/movie')
+def movie():
+    movies = ['spider', 'endgame', 'parasite', 'aladin']
+    return render_template('movie.html', movies=movies)
+    
 
 if __name__ == '__main__':
     app.run(debug=True) # debug모드여야 반영 >> 수정시 서버 계속 열었다 닫았다
